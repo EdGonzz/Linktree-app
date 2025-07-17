2c2c26a11ac4e8d1107b3c13b811acc06402209b
Edwin Contreras <ed25gonz@gmail.com>
2025-07-17 15:50:44 -0400

    configuracion webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  modile: {
+    rules: [
+      {
+        test: /\.js$/,
+        excluse: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './src/index.html',
+      filename: 'index.html'
+    })
+  ]
+}
