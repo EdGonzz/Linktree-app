4ccd99d691fbe9766eda734238513807e9af0ee7
Edwin Contreras <ed25gonz@gmail.com>
2025-08-24 22:21:49 -0400

    Actualizar ruta de plantilla en HtmlWebpackPlugin de './src/index.html' a './public/index.html'

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       inject: true,
-      template: './src/index.html',
+      template: './public/index.html',
       filename: 'index.html'
     })
   ]
